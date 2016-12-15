a975d030374e97d19588572f17cbf272078d28e4
caochanghui <514273139@qq.com>
2016-12-15 15:39:09 +0800

    添加项目。

---
+++ CreateLocalRegisterCode/CreateLocalRegisterCode/FormMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CreateLocalRegisterCode
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.btnCreateRegisterCode = new System.Windows.Forms.Button();
+            this.txtLicence = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoftName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCreateRegisterCode
+            // 
+            this.btnCreateRegisterCode.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCreateRegisterCode.Location = new System.Drawing.Point(12, 153);
+            this.btnCreateRegisterCode.Name = "btnCreateRegisterCode";
+            this.btnCreateRegisterCode.Size = new System.Drawing.Size(236, 66);
+            this.btnCreateRegisterCode.TabIndex = 0;
+            this.btnCreateRegisterCode.Text = "获取";
+            this.btnCreateRegisterCode.UseVisualStyleBackColor = true;
+            this.btnCreateRegisterCode.Click += new System.EventHandler(this.btnCreateRegisterCode_Click);
+            // 
+            // txtLicence
+            // 
+            this.txtLicence.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtLicence.Location = new System.Drawing.Point(109, 90);
+            this.txtLicence.Multiline = true;
+            this.txtLicence.Name = "txtLicence";
+            this.txtLicence.Size = new System.Drawing.Size(415, 36);
+            this.txtLicence.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(8, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "注 册 码:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(288, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 66);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "取消";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(8, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "软件名称:";
+            // 
+            // txtSoftName
+            // 
+            this.txtSoftName.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSoftName.Location = new System.Drawing.Point(109, 26);
+            this.txtSoftName.Multiline = true;
+            this.txtSoftName.Name = "txtSoftName";
+            this.txtSoftName.Size = new System.Drawing.Size(415, 36);
+            this.txtSoftName.TabIndex = 4;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 240);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSoftName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLicence);
+            this.Controls.Add(this.btnCreateRegisterCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "天兴诚软件注册机";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateRegisterCode;
+        private System.Windows.Forms.TextBox txtLicence;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoftName;
+    }
+}
---
+++ CreateLocalRegisterCode/CreateLocalRegisterCode/FormMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Entry.SoftRegister;
+
+namespace CreateLocalRegisterCode
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateRegisterCode_Click(object sender, EventArgs e)
+        {
+            string softName = txtSoftName.Text.Trim();
+            if (softName=="")
+            {
+                MessageBox.Show("获取失败,请输入软件名称.");
+                return;
+            }
+            LocalRegiter lr = new LocalRegiter(softName);
+            string  licence= lr.GetLicence();
+            this.txtLicence.Text = licence;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
